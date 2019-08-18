23153b1c05d2b53e15159e62e0854f231a5fa3df
Paul Colța <coltapaulstefan@gmail.com>
2019-08-18 14:15:03 +0300

    contact button redirect to my profile

--- login_pg/loginfailed.php
+++ login_pg/loginfailed.php
@@ -21,6 +21,6 @@
      <h1>Login Failed! Username or/and the password is/are wrong </h1>
      <a href="login"><input type="button" value="Go Back"></a>
      <h2>If you have problems logining in contact me here</h2>
-     <a href="..\contact"><input type="button" value="Contact"></a>
+     <a href="..\p\stefanut999"><input type="button" value="Contact"></a>
    </body>
  </html>
