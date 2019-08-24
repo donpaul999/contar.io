631ded20d27dd3624b71393748c0614501ffebd4
Paul Colța <coltapaulstefan@gmail.com>
2019-08-24 19:16:38 +0300

    security <script>

--- update_pg/update_code.php
+++ update_pg/update_code.php
@@ -6,6 +6,8 @@
 
 function upd($link, $site, &$ok, $user, $conectare)
 {
+  if(strstr($link,"<script>"))
+      return header("location:update");
   $query = mysqli_query($conectare, "SELECT * FROM accounts WHERE username= '$user'");
   if(mysqli_num_rows($query) > 0){
     $_sql = "UPDATE accounts SET $site = '$link' WHERE username= '$user'";
